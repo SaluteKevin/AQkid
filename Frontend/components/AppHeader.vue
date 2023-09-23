84b7754826fb8bd613c0c4788890eeca6efc6b5d
ingfosbreak <ingfosbreak@outlook.com>
2023-09-23 20:09:46 +0700

    Add: login and register page

--- Frontend/components/AppHeader.vue
+++ Frontend/components/AppHeader.vue
@@ -6,22 +6,24 @@
             <div class="flex flex-wrap items-center justify-between gap-6 py-2">
                 <div class="flex w-full justify-between md:px-0 lg:w-max">
 
-                    <a href="/" aria-label="logo" class="flex items-center space-x-2">
+                    <NuxtLink class="flex items-center space-x-2" to="/">
+                        
                         <div aria-hidden="true" class="flex space-x-1">
                             <img src="/images/AQKids_logo.png" class="h-12">
                         </div>
                         <span class="text-4xl font-bold text-cyan-900">AQKids</span>
-                    </a>
+                    </NuxtLink>
                     
                 </div>
                 
                 <div class="space-x-4">
-                    <a href="/" class="py-2 px-4 text-white font-semibold rounded-md bg-cyan-900">
-                        login
-                    </a>
-                    <a href="/" class="py-2 px-4 text-white font-semibold rounded-md bg-orange-500">
-                        register
-                    </a>
+                    <NuxtLink class="py-2 px-4 text-white font-semibold rounded-md bg-cyan-900" to="/auths/login">
+                    Login
+                    </NuxtLink>
+                    <NuxtLink class="py-2 px-4 text-white font-semibold rounded-md bg-orange-500" to="/auths/register">
+                    Register
+                    </NuxtLink>
+                    
                 </div>
                 
             </div>
