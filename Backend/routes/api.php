4f5f2c16668d3448f030138296bc4810f4aaa248
ingfosbreak <ingfosbreak@outlook.com>
2023-09-23 14:28:12 +0700

    Update: JWT Auth

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -16,28 +16,43 @@ use App\Models\User;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    // $request->user()->info;
-    // return $request->user();
-    return User::with('info')->get();
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//      $request->user()->info;
+//      return $request->user();
+//     return User::with('info')->get();
+// });
+
+// Route::middleware(['auth:sanctum','multirole:staff'])->get('/test', function (Request $request) {
+//     return [ 'success' => true ];
+// });
+
+
+// Route::middleware(['auth:sanctum'])->post('/revoke', function (Request $request) {
+//     $user = auth()->user();
+
+//     // Revoke all of the user's tokens
+//     $user->tokens->each->delete();
+// });
+
+
+// Route::post('/loginServer', [AuthController::class,'loginServer'])->name('login.server');
+// Route::get('/setApiCookie', [AuthController::class,'setApiCookie'])->name('cookie.server');
+
+// Route::post('/testFETCH', function () {
+//     return ['success' => true];
+// })->name('test.server');
+
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+
 });
-
-Route::middleware(['auth:sanctum','multirole:staff'])->get('/test', function (Request $request) {
-    return [ 'success' => true ];
-});
-
-
-Route::middleware(['auth:sanctum'])->post('/revoke', function (Request $request) {
-    $user = auth()->user();
-
-    // Revoke all of the user's tokens
-    $user->tokens->each->delete();
-});
-
-
-Route::post('/loginServer', [AuthController::class,'loginServer'])->name('login.server');
-Route::get('/setApiCookie', [AuthController::class,'setApiCookie'])->name('cookie.server');
-
-Route::post('/testFETCH', function () {
-    return ['success' => true];
-})->name('test.server');
