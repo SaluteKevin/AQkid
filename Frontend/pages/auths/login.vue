3e9fc2a9da73949a0511aca32519bab2dc917fc1
oOTEEOo <thanadon.kr@ku.th>
2023-10-02 14:28:03 +0700

    fix Login UI little bit again

--- Frontend/pages/auths/login.vue
+++ Frontend/pages/auths/login.vue
@@ -1,157 +1,85 @@
 <template>
-    <section class=" ">
-  <!-- <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-
-      <div class="w-full bg-cyan-900 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-orange-500 md:text-2xl">
-                  Sign in to your account
-              </h1>
-              <form class="space-y-4 md:space-y-6" @submit.prevent="handleLogin()">
-                  <div>
-                      <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="username" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
-                  </div>
-                  <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
-                  </div>
-                  <div class="flex items-center justify-between">
-                      <div class="flex items-start">
-                          <div class="flex items-center h-5">
-                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
-                          </div>
-                          <div class="ml-3 text-sm">
-                            <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
-                          </div>
-                      </div>
-                      <NuxtLink class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500" to="/auths/forgot">Forgot password?</NuxtLink>
-                      
-                  </div>
-                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don’t have an account yet? <NuxtLink class="font-medium text-primary-600 hover:underline dark:text-primary-500" to="/auths/register">Sign up</NuxtLink>
-                  </p>
-              </form>
-          </div>
-      </div>
-  </div> -->
-  <!-- source:https://codepen.io/owaiswiz/pen/jOPvEPB -->
-<div class="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
-    <div class="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
-        <div class="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
-            <div class="flex">
-                
-                <img src="/images/AQKids_logo.png"
-                    class="w-1/2"/>
-                <div class="flex text-6xl font-bold text-cyan-900 justify-center items-center">
-                    AquaKids
+    
+    <div class="min-h-screen bg-gray-100 text-gray-900 flex justify-center px-20">
+        <div class="m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+            <div class="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+                <div class="flex">
+                    
+                    <img src="/images/AQKids_logo.png"
+                        class="w-1/3"/>
+                    <div class="flex text-6xl font-bold text-cyan-900 justify-center items-center">
+                        AquaKids
+                    </div>
                 </div>
-            </div>
-            <div class="mt-12 flex flex-col items-center">
-                <div class="w-full flex-1 mt-8">
-                    <div class="flex flex-col items-center">
-                        <button
-                            class="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-orange-500 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
-                            <div class="bg-white p-2 rounded-full">
-                                <svg class="w-4" viewBox="0 0 533.5 544.3">
-                                    <path
-                                        d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"
-                                        fill="#4285f4" />
-                                    <path
-                                        d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z"
-                                        fill="#34a853" />
-                                    <path
-                                        d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z"
-                                        fill="#fbbc04" />
-                                    <path
-                                        d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z"
-                                        fill="#ea4335" />
+                <div class="mt-12 flex flex-col items-center">
+                    <form class="w-full flex-1 mt-8" @submit.prevent="handleLogin()">
+                        <div class="mx-auto max-w-xs">
+                            <input
+                                class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                                type="text" name="username" placeholder="Email or Username" />
+                            <input
+                                class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                                type="password" name="password" placeholder="Password" />
+                            <button type="submit"
+                                class="mt-5 tracking-wide font-semibold bg-orange-500 text-white-500 w-full py-4 rounded-lg hover:bg-green-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                                <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                                    <circle cx="8.5" cy="7" r="4" />
+                                    <path d="M20 8v6M23 11h-6" />
                                 </svg>
-                            </div>
-                            <span class="ml-4">
-                                Sign In with Google
-                            </span>
-                        </button>
-
-                    </div>
-
-                    <div class="my-12 border-b text-center">
-                        <div
-                            class="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
-                            Or sign In with Cartesian E-mail
+                                <span class="ml-">
+                                    Sign In
+                                </span>
+                            </button>
+                            <p class="text-sm font-light text-gray-500 dark:text-gray-400 pt-2">
+                                Don't have an account yet? <NuxtLink class="font-medium text-primary-600 hover:underline dark:text-primary-500" to="/auths/register">Sign up</NuxtLink>
+                            </p>
                         </div>
-                    </div>
-
-                    <div class="mx-auto max-w-xs">
-                        <input
-                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                            type="email" placeholder="Email" />
-                        <input
-                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                            type="password" placeholder="Password" />
-                        <button
-                            class="mt-5 tracking-wide font-semibold bg-orange-500 text-white-500 w-full py-4 rounded-lg hover:bg-green-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-                            <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
-                                stroke-linecap="round" stroke-linejoin="round">
-                                <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
-                                <circle cx="8.5" cy="7" r="4" />
-                                <path d="M20 8v6M23 11h-6" />
-                            </svg>
-                            <span class="ml-">
-                                Sign In
-                            </span>
-                        </button>
-                        <p class="text-sm font-light text-gray-500 dark:text-gray-400 pt-2">
-                            Don't have an account yet? <NuxtLink class="font-medium text-primary-600 hover:underline dark:text-primary-500" to="/auths/register">Sign up</NuxtLink>
-                        </p>
-                    </div>
+                    </form>
                 </div>
             </div>
-        </div>
-        <div class="flex-1 bg-orange-500 text-center hidden lg:flex rounded-r-lg">
-            <!-- <div class="w-full bg-cover bg-center "
-                style="background-image: url('https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/362288650_738993328237417_988681206543655468_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=49d041&_nc_eui2=AeHKvyMFkp3zIBK4UdJbhACtxwiiq0roXf_HCKKrSuhd_4z3wzy3lklqvECuReZ1JRmKzwXf41fw4gQIwL1OS0LE&_nc_ohc=-WswNL9jDvgAX8WPqtn&_nc_ht=scontent-sin6-2.xx&oh=00_AfCszx782q0zHh9smqJrqG798172pAZdXGAZoBkgOBxWoA&oe=651FA667');">
-            </div> -->
-            <img class="w-full object-cover rounded-r-lg" src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/362288650_738993328237417_988681206543655468_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=49d041&_nc_eui2=AeHKvyMFkp3zIBK4UdJbhACtxwiiq0roXf_HCKKrSuhd_4z3wzy3lklqvECuReZ1JRmKzwXf41fw4gQIwL1OS0LE&_nc_ohc=-WswNL9jDvgAX8WPqtn&_nc_ht=scontent-sin6-2.xx&oh=00_AfCszx782q0zHh9smqJrqG798172pAZdXGAZoBkgOBxWoA&oe=651FA667">
-        
+            <div class="flex-1 bg-orange-500 text-center hidden lg:flex rounded-r-lg">
+               
+                <img class="w-full object-cover rounded-r-lg" src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/362288650_738993328237417_988681206543655468_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=49d041&_nc_eui2=AeHKvyMFkp3zIBK4UdJbhACtxwiiq0roXf_HCKKrSuhd_4z3wzy3lklqvECuReZ1JRmKzwXf41fw4gQIwL1OS0LE&_nc_ohc=-WswNL9jDvgAX8WPqtn&_nc_ht=scontent-sin6-2.xx&oh=00_AfCszx782q0zHh9smqJrqG798172pAZdXGAZoBkgOBxWoA&oe=651FA667">
+            
+            </div>
         </div>
     </div>
-</div>
-</section>
-
-</template>
-
-
-<script setup lang="ts">
-
-import {useAuthStore} from "~/stores/useAuthStore";
-
-const auth = useAuthStore();
-
-const form = ref({
-  username: "test",
-  password: "123"
-});
-
-async function handleLogin() {
-
-   
-    await useApiFetch("/sanctum/csrf-cookie");
-
-    const {data: login, error: loginerror } = await useApiFetch("/api/loginServer", {
-      method: "POST",
-      body: {
-        username: "test",
-        password: "123"
-      },
+    
+    
+    </template>
+    
+    
+    <script setup lang="ts">
+    
+    import {useAuthStore} from "~/stores/useAuthStore";
+    
+    const auth = useAuthStore();
+    
+    const form = ref({
+      username: "test",
+      password: "123"
     });
-
-    if (loginerror.value) {
-        console.log(loginerror.value.data.message);
+    
+    async function handleLogin() {
+    
+       
+        await useApiFetch("/sanctum/csrf-cookie");
+    
+        const {data: login, error: loginerror } = await useApiFetch("/api/loginServer", {
+          method: "POST",
+          body: {
+            username: "test",
+            password: "123"
+          },
+        });
+    
+        if (loginerror.value) {
+            console.log(loginerror.value.data.message);
+        }
+    
     }
-
-}
-
-
-</script>
+    
+    
+    </script>
