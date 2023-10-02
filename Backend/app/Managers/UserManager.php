dd19b1902a47e0d0515e6196a2cbd01138b74c6e
ingfosbreak <ingfosbreak@outlook.com>
2023-10-02 15:17:17 +0700

    Update: User and mail

--- Backend/app/Managers/UserManager.php
+++ Backend/app/Managers/UserManager.php
@@ -6,9 +6,7 @@ use App\Models\User;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
-use App\Models\PasswordResetToken;
 use Tymon\JWTAuth\Facades\JWTAuth;
-use App\Services\MailService;
 use Illuminate\Contracts\Validation\Factory as ValidationFactory;
 
 
@@ -36,69 +34,6 @@ class UserManager {
     }
 
 
-    public function verifyUser($userId) {
-
-        $user = User::find($userId);
-        $user->email_verified_at = Carbon::now();
-        
-        if ($user->save()) {
-            return true;
-        }
-
-        return response()->json(['message' => "Can't verify your email."], 422);
-
-
-
-    }
-
-    public function forgotPassword(Request $request) {
-
-        if ($this->isEmail($request->get('email'))) {
-            
-            $user = User::where('email',$request->get('email'))->first();
-            
-            if ($user != null) {
-
-                if ($user->email_verified_at != null) {
-                    //sent mail
-
-                    MailService::getMailManager()->sendResetPasswordMail($user->email);
-
-                    return response()->json(['message' => "We have sent you a ResetPassword Email"]);
-                }
-
-                return response()->json(['message' => "Oops, Sorry your account email is not verify."], 422);
-
-
-            }
-
-            return response()->json(['message' => "Oops, Sorry we can't find your email account."], 422);
-
-        }
-
-        else {
-
-            $user = User::where('username',$request->get('email'))->first();
-            
-            if ($user != null) {
-
-                if ($user->email_verified_at != null) {
-                    //sent mail
-
-                    MailService::getMailManager()->sendResetPasswordMail($user->email);
-
-                    return response()->json(['message' => "We have sent you a ResetPassword Email"]);
-                }
-
-                return response()->json(['message' => "Oops, Sorry your account email is not verify."], 422);
-
-            }
-
-            return response()->json(['message' => "Oops, Sorry we can't find your username account."], 422);
-
-        }
-
-    }
 
     public function isEmail($param) {
 
@@ -111,55 +46,7 @@ class UserManager {
 
     }
 
-    public function resetPassword(Request $request, $token) {
-        
-        $token = PasswordResetToken::where('token', $token)->first();
-
-        if ($token != null) {
-
-            $user = User::where('email', $token->email)->first();
-
-            if ($user != null) {
-
-                $user->password = $request->get('password');
-                
-                if ($user->save()) {
-                    return true;
-                }
-
-                return response()->json(['message' => "Oops, It looks like there is a problem in process of reset your password."], 422);
-
-            }
-
-            return response()->json(['message' => "Oops, Sorry We can't find your account."], 422);
-            
-        }
-        
-        return response()->json(['message' => "Oops, It looks like your token has expired."], 422);
-
-
-    }
-
-    public function removePasswordResetToken($token) {
-        
-        $token = PasswordResetToken::where('token', $token)->first();
-
-        if ($token != null) {
-
-            if ($token->delete()) {
-
-                return true;
-
-            }
-
-            return response()->json(['message' => "Oops, It looks like there is a problem in process of reseting your password."], 422);
-            
-
-        }
-
-        return response()->json(['message' => "Oops, It looks like your token has expired."], 422);
-
-    }
+    
     
 
 
