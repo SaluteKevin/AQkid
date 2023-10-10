dc11b213ae3bee7d7397d297e2ccf4e07ea729ec
SaluteKevin <salute.k@ku.th>
2023-10-10 21:38:31 +0700

    Add: layout home page for staff, path to teacher list at side bar

--- Frontend/pages/staff/index.vue
+++ Frontend/pages/staff/index.vue
@@ -1,5 +1,15 @@
 <template>
+<div class="grid grid-rows-5 grid-flow-col">
+    <div class="bg-rose-300 row-span-5 col-span-4 h-96 w-full">
 
+    </div>
+    <div class="bg-amber-300 row-span-2 col-span-2 h-48 w-full">
+
+    </div>
+    <div class="bg-green-300 row-span-3 col-span-2 h-48 w-full">
+
+    </div>
+</div>
 </template>
 
 <script setup lang="ts">
