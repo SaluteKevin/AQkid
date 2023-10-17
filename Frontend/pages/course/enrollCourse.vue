abbe6d10c9ac3c21c545cdeeb0556575d8fe2ece
ingfosbreak <ingfosbreak@outlook.com>
2023-10-17 17:06:59 +0700

    Update: enrollCourse

--- Frontend/pages/course/enrollCourse.vue
+++ Frontend/pages/course/enrollCourse.vue
@@ -1,150 +1,31 @@
 <template>
-
-    <div class="py-5 bg-cyan-900">
-        <p class="text-center text-6xl text-gray-900 dark:text-white">Enroll Course</p>
+    <div class='app p-12 transform ease-in-out'>
+        <FullCalendar :options='calendarOptions' />
     </div>
 
-    <div class="h-screen flex justify-center p-72 items-center flex-col">
-        <div class="shadow-2xl border broder-1 w-fit flex flex-col p-12 rounded-2xl">
-        <div class="grid grid-rows-8 grid-cols-10 gap-1 ">
-            <div class="pr-3 content-center justify-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    time
-                </h5>
+    <div class="flex justify-center">
+        <article
+            class="w-96 relative flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto ">
+            <img src="https://www.ecohome.net/media/articles/images/f2/9c/f29c921116e7b42501639bda77885d5051a5c8f1/thumbs/dOp8pM6ckCwo_1200x500_EE_R0wWf.jpg"
+                alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
+            <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+            <h3 id="day" class="z-10 text-3xl font-bold text-white">{{ dayy }}</h3>
+            <h3 id="time" class="z-10 text-3xl font-bold text-white">{{ timee }}</h3>
+            <div class="z-10 gap-y-1 right-0 overflow-hidden text-sm leading-6 text-gray-300 p-2">
+                <NuxtLink :to="`/course/confirmCourse${idCourse}`"
+                    class="rounded-r-lg group relative px-8 py-1 overflow-hidden bg-white text-xl shadow my-6">
+                    <div
+                        class="absolute inset-0 w-3 bg-lime-600 transition-all duration-[250ms] ease-out group-hover:w-full rounded-r-lg">
+                    </div>
+                    <span class="relative text-black group-hover:text-white ">Confirm</span>
+                </NuxtLink>
             </div>
-            <div class="col-span-9 row-span-1 ml-2">
-                <div class="grid grid-rows-1 grid-cols-9">
-
-                    <div class="content-center justify-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            9.00
-                        </h5>
-                    </div>
-                    <div class="content-center justify-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            10.00
-                        </h5>
-                    </div>
-                    <div class="content-center justify-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            11.00
-                        </h5>
-                    </div>
-                    <div class="content-center justify-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            12.00
-                        </h5>
-                    </div>
-                    <div class="content-center justify-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            13.00
-                        </h5>
-                    </div>
-                    <div class="content-center justify-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            14.00
-                        </h5>
-                    </div>
-                    <div class="content-center justify-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            15.00
-                        </h5>
-                    </div>
-                    <div class="content-center justify-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            16.00
-                        </h5>
-                    </div>
-                    <div class="content-center justify-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            17.00
-                        </h5>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-        
-        <div class="grid grid-rows-7 grid-cols-10 py-4 gap-1  ">
-            
-            <div class="col-span-1 row-span-7">
-                <div class="my-2 content-center justify-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Monday
-                    </h5>
-                </div>
-                <div class="my-2 content-center justify-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Tuesday
-                    </h5>
-                </div>
-                <div class="my-2 content-center justify-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Wednesday
-                    </h5>
-                </div>
-                <div class="my-2 content-center justify-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <h5 class="text-center mb-2 pb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Thursday
-                    </h5>
-                </div>
-                <div class="my-2 content-center justify-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <h5 class="text-center mb-2 pb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Friday
-                    </h5>
-                </div>
-                <div class="my-2 content-center justify-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Saturday
-                    </h5>
-                </div>
-                <div class="my-2 content-center justify-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Sunday
-                    </h5>
-                </div>
-                
-            </div>
-            <div class="col-span-9 row-span-7">
-                <div class="grid grid-rows-7 grid-cols-9">
-                    <div class="ml-2 mb-3 mt-3 col-span-9 w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <h5 class="text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Close
-                        </h5>
-                    </div>
-                    <div v-for="day_time in day_times" v-bind:key="day_time">
-                        <CourseTimeCourse @select="selectCourse" :day=day_time.day :time=day_time.time :id=day_time.id :status=day_time.status></CourseTimeCourse>
-                
-                    </div>
-                
-                </div>
-            </div>
-
-        </div>
+        </article>
     </div>
-        <div class="flex justify-center">
-            <article class="w-96 relative flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto ">
-                <img src="https://www.ecohome.net/media/articles/images/f2/9c/f29c921116e7b42501639bda77885d5051a5c8f1/thumbs/dOp8pM6ckCwo_1200x500_EE_R0wWf.jpg" alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
-                <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-                <h3 id="day" class="z-10 text-3xl font-bold text-white">{{dayy}}</h3>
-                <h3 id="time" class="z-10 text-3xl font-bold text-white">{{timee}}</h3>
-                <div class="z-10 gap-y-1 right-0 overflow-hidden text-sm leading-6 text-gray-300 p-2">
-                    <NuxtLink :to="`/course/confirmCourse${idCourse}`" class="rounded-r-lg group relative px-8 py-1 overflow-hidden bg-white text-xl shadow my-6">
-                            <div class="absolute inset-0 w-3 bg-lime-600 transition-all duration-[250ms] ease-out group-hover:w-full rounded-r-lg"></div>
-                            <span class="relative text-black group-hover:text-white ">Confirm</span>
-                        </NuxtLink>
-                </div>
-            </article>
-            </div>
-        </div>
-        
-        
-        
-        
-        
-    </template>
+</template>
 
 <script setup lang="ts">
+
 const dayy = ref('please select the time')
 const timee = ref('')
 const idCourse = ref(0)
@@ -152,49 +33,96 @@ const days = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
 const times = ['9.00', '10.00', '11.00', '12.00', '13.00', '14.00', '15.00', '16.00', '17.00']
 
 
-const selectCourse = (day: string, time: string, id: number) => {
-    console.log(day, time, id);
-    dayy.value = day
-    timee.value = time
-    idCourse.value = id
+// const selectCourse = (day: string, time: string, id: number) => {
+//     console.log(day, time, id);
+//     dayy.value = day
+//     timee.value = time
+//     idCourse.value = id
+// }
+
+
+// const day_times = ref([])
+
+// for (let day in days) {
+//     for(let time in times){
+//         const temp = ref({
+//             day: days[day],
+//             time: times[time],
+//             status: false,
+//             id: 0,
+//         })
+//         day_times.value.push(temp.value)
+//     }
+// }
+
+// console.log(day_times.value[0].day);
+
+
+// const timeArrays = [
+//     {
+//         day: "MON",
+//         time: "123",
+//         status: false
+//     },
+//     {
+//         day: "MON",
+//         time: "123",
+//         status: false
+//     },
+//     {
+//         day: "MON",
+//         time: "123",
+//         status: false
+//     },
+
+// ]
+
+import FullCalendar from '@fullcalendar/vue3'
+import interactionPlugin from '@fullcalendar/interaction'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+
+const calendarOptions = ref({
+    plugins: [interactionPlugin, timeGridPlugin, dayGridPlugin],
+    initialView: 'timeGridWeek',
+    nowIndicator: false,
+    editable: true,
+    dayHeaderFormat: { weekday: 'long' },
+    scrollTime: '10:00:00', // Set the scrollTime to 10:00 AM (optional)
+    slotMinTime: '10:00:00', // Set the minimum time to 10:00 AM (optional)
+    slotMaxTime: '18:00:00', // 
+    slotLabelFormat: {
+        hour: 'numeric',
+        minute: '2-digit',
+        omitZeroMinute: false,
+    },
+    contentHeight: 'auto',
+
+    //   dateClick: handleDateClick,
+    //   eventClick: handleEventClick,
+    //   eventMouseEnter: handleEventhover,
+    //   eventMouseLeave: handleEventLeave,
+
+    events: ref([]),
+    eventColor: '#378006'
+})
+
+
+const eventData = ref([
+    { title: 'ink bd', date: '2023-12-19', id: 123, color: 'purple', description: 'description for Repeating Event', eventClassNames: "test" },
+    { title: 'event 1', date: '2023-12-19' },
+    { title: 'event 1', date: '2023-12-19' },
+    { title: 'event 1', date: '2023-12-19' },
+    { title: 'event 2', date: '2023-09-12' }
+])
+
+
+if (eventData.value) {
+    calendarOptions.value.events = eventData;
+
 }
 
-
-const day_times = ref([])
-
-for (let day in days) {
-    for(let time in times){
-        const temp = ref({
-            day: days[day],
-            time: times[time],
-            status: false,
-            id: 0,
-        })
-        day_times.value.push(temp.value)
-    }
-}
-
-console.log(day_times.value[0].day);
-
-
-const timeArrays = [
-    {
-        day: "MON",
-        time: "123",
-        status: false
-    },
-    {
-        day: "MON",
-        time: "123",
-        status: false
-    },
-    {
-        day: "MON",
-        time: "123",
-        status: false
-    },
-
-]
 
 
 
--- Frontend/pages/staff/detail/student[id].vue
+++ Frontend/pages/staff/detail/student[id].vue
@@ -1,50 +1,51 @@
 <template>
+    <div class="min-h-screen bg-gradient-to-l to-purple-50 to-60% from-[#bce1ff] from-10%">
+        <div class="container mx-auto py-8">
+            <div class="grid grid-cols-4 sm:grid-cols-12 gap-6 px-4">
+                <div class="col-span-4 sm:col-span-3">
+                    <div class="bg-white shadow rounded-lg p-6">
+                        <div class="flex flex-col items-center">
+                            <img src="https://randomuser.me/api/portraits/men/94.jpg"
+                                class="w-32 h-32 bg-gray-300 rounded-full mb-4 shrink-0">
 
-<div class="min-h-screen bg-gradient-to-l to-purple-50 to-60% from-[#bce1ff] from-10%">
-    <div class="container mx-auto py-8">
-        <div class="grid grid-cols-4 sm:grid-cols-12 gap-6 px-4">
-            <div class="col-span-4 sm:col-span-3">
-                <div class="bg-white shadow rounded-lg p-6">
-                    <div class="flex flex-col items-center">
-                        <img src="https://randomuser.me/api/portraits/men/94.jpg" class="w-32 h-32 bg-gray-300 rounded-full mb-4 shrink-0">
 
-                        
-                        <h1 class="text-xl font-bold">{{student.first_name}}</h1>
-                        <p class="text-gray-600">{{student.role}}</p>
+                            <h1 class="text-xl font-bold">{{ student.first_name }}</h1>
+                            <p class="text-gray-600">{{ student.role }}</p>
 
-                        
-                       
-                    </div>
-                    <hr class="my-6 border-t border-gray-300">
-                    <div class="mt-6  justify-center">
+
+
+                        </div>
+                        <hr class="my-6 border-t border-gray-300">
+                        <div class="mt-6  justify-center">
                             <p class="text-black font-bold">Phone No.</p>
                             <h2>{{ student.phone_number }}</h2>
                             <h2 v-if="!student.phone_number">No Phone No.</h2>
-                    </div>
-                    <hr class="my-6 border-t border-gray-300">
-                    <div class="mt-6  justify-center">
+                        </div>
+                        <hr class="my-6 border-t border-gray-300">
+                        <div class="mt-6  justify-center">
                             <p class="text-black font-bold">Email</p>
                             <h2>{{ student.email }}</h2>
                             <h2 v-if="!student.email">No Email</h2>
+                        </div>
+
+
                     </div>
-                    
-                    
                 </div>
-            </div>
-            <div class="col-span-4 sm:col-span-9">
-                <div class="bg-white shadow rounded-lg p-6">
-                    <h2 class="text-xl font-bold mb-4">Student Information</h2>
-                    <p class="text-gray-700">Firstname : {{student.first_name}} </p>
-                    <p class="text-gray-700">Middlename : {{student.middle_name}} </p>
-                    <p class="text-gray-700">Lastname : {{student.last_name}} </p>
-                    <p class="text-gray-700">Age : {{new Date().getFullYear() - new Date(student.birthdate).getFullYear()}} </p>
-                    <hr class="my-6 border-t border-gray-300">
-                    
+                <div class="col-span-4 sm:col-span-9">
+                    <div class="bg-white shadow rounded-lg p-6">
+                        <h2 class="text-xl font-bold mb-4">Student Information</h2>
+                        <p class="text-gray-700">Firstname : {{ student.first_name }} </p>
+                        <p class="text-gray-700">Middlename : {{ student.middle_name }} </p>
+                        <p class="text-gray-700">Lastname : {{ student.last_name }} </p>
+                        <p class="text-gray-700">Age : {{ new Date().getFullYear() - new
+                            Date(student.birthdate).getFullYear() }} </p>
+                        <hr class="my-6 border-t border-gray-300">
 
-                    <h2 class="text-xl font-bold mt-6 mb-4">Courses</h2>
 
-                    
-                    <!-- <div class="mb-3 shadow-xl border rounded-2xl p-4" v-for="course in teacher.courses" :key="course.id">
+                        <h2 class="text-xl font-bold mt-6 mb-4">Courses</h2>
+
+
+                        <!-- <div class="mb-3 shadow-xl border rounded-2xl p-4" v-for="course in teacher.courses" :key="course.id">
                         <div class="flex justify-between  p-4 rounded-lg">
                             <span class="text-gray-600 font-bold">{{ course.title }}</span>
                             <p class="flex flex-col">
@@ -55,24 +56,24 @@
                             
                         </div>
                     </div> -->
-                    
-                    
-                    
+
+
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </template>
 
 <script setup lang="ts">
-definePageMeta({layout: "staff"})
+definePageMeta({ layout: "staff" })
 
 const route = useRoute();
 
 const student = ref({})
 
-const {data: studentResponse, error: studentError } = await useApiFetch(`api/staff/students/${route.params.id}`, {});
+const { data: studentResponse, error: studentError } = await useApiFetch(`api/staff/students/${route.params.id}`, {});
 
 if (studentResponse.value) {
     student.value = studentResponse.value;
