75bbf3c6ed1aee8496baf808e21b67c359767bf2
ingfosbreak <ingfosbreak@outlook.com>
2023-10-26 21:41:00 +0700

    Fix: bug fix and layout logout feature

--- Frontend/layouts/teacher.vue
+++ Frontend/layouts/teacher.vue
@@ -17,7 +17,7 @@
                 </div>
                 <div class="flex items-center justify-end gap-3">
                     <NuxtLink class="hidden items-center justify-center rounded-xl bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-all duration-150 hover:bg-gray-50 sm:inline-flex"
-                        to="/student/editprofile">{{ auth.user.value.username }}</NuxtLink>
+                        to="/auths/editprofile">{{ auth.user.value.username }}</NuxtLink>
                     <button v-on:click='logout' class="inline-flex items-center justify-center rounded-xl bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
                        >Logout</button>
                 </div>
@@ -30,8 +30,6 @@
 </template>
 
 <script setup lang="ts">
-
-
 
 import {useAuthStore} from "~/stores/useAuthStore";
 const auth = useAuthStore();
