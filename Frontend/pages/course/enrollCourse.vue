b62ad7cd09f03f2bebea453ef26098fc2fc20fa9
ingfosbreak <ingfosbreak@outlook.com>
2023-10-27 17:13:01 +0700

    Add: new enroll course page

--- Frontend/pages/course/enrollCourse.vue
+++ Frontend/pages/course/enrollCourse.vue
@@ -1,36 +1,65 @@
 <template>
     <div class='app p-12 transform ease-in-out'>
-        <FullCalendar v-click-outside="clickOutside" :options='calendarOptions' />
+        <FullCalendar :options='calendarOptions' />
+    </div>
+    <div  ref="scrollEvent" v-if="showCard" class="flex w-full justify-center mb-8">
+        <div class="bg-white max-w-2xl shadow overflow-hidden sm:rounded-lg">
+            <div class="px-4 py-5 sm:px-6">
+                <h3 class="text-lg leading-6 font-medium text-gray-900">
+                    Course Info
+                </h3>
+                <p class="mt-1 max-w-2xl text-sm text-gray-500">
+                    Details and informations about course.
+                </p>
+            </div>
+            <div class="border-t border-gray-200">
+                <dl>
+                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">
+                            Day
+                        </dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {{ day }}
+                        </dd>
+                    </div>
+                    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">
+                            Time
+                        </dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {{ time }}
+                        </dd>
+                    </div>
+                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">
+                            Already in
+                        </dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            <div v-if="author">Yes</div>
+                            <div v-if="!author">No</div>
+                        </dd>
+                    </div>
+
+                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        
+                        <NuxtLink v-if="!author" :to="`/course/confirmCourse${idCourse}`"
+                            class="rounded-lg group relative px-8 py-1 overflow-hidden bg-green-300 hover:bg-green-400 focus:bg-green-500 text-xl shadow my-6">
+                            Confirm
+                        </NuxtLink>
+
+                        <button v-on:click="clickOutside" class="rounded-lg group relative px-8 py-1 overflow-hidden bg-red-300 hover:bg-red-400 focus:bg-red-500 text-xl shadow my-6">Cancel</button>
+                    </div>
+                </dl>
+            </div>
+        </div>
     </div>
 
-    <div v-if="showCard" class="flex justify-center">
-        <article
-            class="w-96 relative flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto ">
-            <img src="https://www.ecohome.net/media/articles/images/f2/9c/f29c921116e7b42501639bda77885d5051a5c8f1/thumbs/dOp8pM6ckCwo_1200x500_EE_R0wWf.jpg"
-                alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
-            <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-            <h3 id="day" class="z-10 text-3xl font-bold text-white">{{ day }}</h3>
-            <h3 id="time" class="z-10 text-3xl font-bold text-white">{{ time }}</h3>
-            <div class="z-10 gap-y-1 right-0 overflow-hidden text-sm leading-6 text-gray-300 p-2">
-                <NuxtLink :to="`/course/confirmCourse${idCourse}`"
-                    class="rounded-r-lg group relative px-8 py-1 overflow-hidden bg-white text-xl shadow my-6">
-                    <div
-                        class="absolute inset-0 w-3 bg-lime-600 transition-all duration-[250ms] ease-out group-hover:w-full rounded-r-lg">
-                    </div>
-                    <span class="relative text-black group-hover:text-white ">Confirm</span>
-                </NuxtLink>
-            </div>
-        </article>
-    </div>
+
 </template>
 
 <script setup lang="ts">
 
-definePageMeta({layout: "student"})
-
-const day = ref('please select the time')
-const time = ref<any>('')
-const idCourse = ref(0)
+definePageMeta({ layout: "student" })
 
 
 import FullCalendar from '@fullcalendar/vue3'
@@ -40,109 +69,178 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import dayjs from 'dayjs';
 
 const calendarOptions = ref({
-  plugins: [interactionPlugin, timeGridPlugin, dayGridPlugin],
-  initialView: 'timeGridWeek',
-  nowIndicator: true,
-  editable: false,
-//   dateClick: handleDateClick,
-  eventClick: handleEventClick,
-  // eventMouseEnter: handleEventhover,
-  // eventMouseLeave: handleEventLeave,
-  contentHeight: 'auto',
-  events: ref([]),
-  scrollTime: '10:00:00', // Set the scrollTime to 10:00 AM (optional)
-  slotMinTime: '10:00:00', // Set the minimum time to 10:00 AM (optional)
-  slotMaxTime: '18:00:00', // 
-  slotLabelFormat: {
-    hour: 'numeric',
-    minute: '2-digit',
-    omitZeroMinute: false,
-  },
-  allDaySlot: false,
-  initialDate: ref(),
-  headerToolbar: {
-    left: 'prev,next today',
-    center: 'title',
-    right: 'dayGridMonth,timeGridWeek,timeGridDay',
-  },
+    plugins: [interactionPlugin, timeGridPlugin, dayGridPlugin],
+    initialView: 'timeGridWeek',
+    nowIndicator: true,
+    editable: false,
+    //   dateClick: handleDateClick,
+    eventClick: handleEventClick,
+    // eventMouseEnter: handleEventhover,
+    // eventMouseLeave: handleEventLeave,
+    contentHeight: 'auto',
+    events: ref([]),
+    scrollTime: '10:00:00', // Set the scrollTime to 10:00 AM (optional)
+    slotMinTime: '10:00:00', // Set the minimum time to 10:00 AM (optional)
+    slotMaxTime: '18:00:00', // 
+    slotLabelFormat: {
+        hour: 'numeric',
+        minute: '2-digit',
+        omitZeroMinute: false,
+    },
+    allDaySlot: false,
+    initialDate: ref(),
+    headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay',
+    },
 })
 
 
 
-const eventData = ref([
-    
-])
+const eventData = ref([])
 
 
-import {useAuthStore} from "~/stores/useAuthStore";
+import { useAuthStore } from "~/stores/useAuthStore";
 const user = useAuthStore().user;
 
 
 // all courses
-const {data: eventCourse, error: loginError } = await useApiFetch("api/student/getAllCourses", {});
+const { data: eventCourse, error: eventError } = await useApiFetch(`api/student/allEnrollCourse/${user.value.id}`, {});
 
-if(eventCourse.value){
+if (eventCourse.value) {
     console.log(eventCourse.value);
-    
-    for(const event in eventCourse.value){
-        let ageRange = `${eventCourse.value[event].max_age}-${eventCourse.value[event].min_age}`;
+
+
+
+    for (const event in eventCourse.value) {
+
         let temp = {
             title: eventCourse.value[event].title,
-            start: eventCourse.value[event].start_datetime,
+            start: eventCourse.value[event].starts_on,
             uid: eventCourse.value[event].id,
-            color: 'purple',
-            description: ageRange,
-            
+            datestore: eventCourse.value[event].starts_on,
+            author: eventCourse.value[event].author,
+
         }
-        console.log(temp);
-        
-        eventData.value.push(temp)
-    
+
+        if (dayjs(eventCourse.value[event].starts_on).day() == 0) {
+            temp["color"] = 'red';
+        }
+
+        if (dayjs(eventCourse.value[event].starts_on).day() == 1) {
+            temp["color"] = 'yellow';
+        }
+
+        if (dayjs(eventCourse.value[event].starts_on).day() == 2) {
+            temp["color"] = 'pink';
+        }
+
+        if (dayjs(eventCourse.value[event].starts_on).day() == 3) {
+            temp["color"] = 'green';
+        }
+
+        if (dayjs(eventCourse.value[event].starts_on).day() == 4) {
+            temp["color"] = 'orange';
+        }
+
+        if (dayjs(eventCourse.value[event].starts_on).day() == 5) {
+            temp["color"] = 'blue';
+        }
+
+        if (dayjs(eventCourse.value[event].starts_on).day() == 6) {
+            temp["color"] = 'purple';
+        }
+
+        if (eventCourse.value[event].author) {
+            temp["color"] = 'gray';
+        }
+
+        eventData.value.push(temp);
+
+
     }
-    
+
+
+} else {
+
+    if (eventError.value) {
+
+    }
+
 }
 
 
+calendarOptions.value.events = eventData;
 
 
-if (eventData.value) {
-    calendarOptions.value.events = eventData;
-
-}
 
 const showCard = ref(false);
+const day = ref('please select the time')
+const time = ref<any>('')
+const author = ref(false)
+const idCourse = ref(0)
 
-function  handleEventClick (arg) {
+
+
+
+async function handleEventClick(arg) {
 
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     day.value = daysOfWeek[new Date(arg.event.start).getDay()]
     time.value = formatTime(new Date(arg.event.start))
     idCourse.value = arg.event.extendedProps.uid
-    showCard.value = true;
+    author.value = arg.event.extendedProps.author;
+    
+    await showCardtrue();
+
+    await toEvent();
 
 }
 
 function formatTime(date) {
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
-  const ampm = hours >= 12 ? 'PM' : 'AM';
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
 
-  // Convert 24-hour format to 12-hour format
-  const formattedHours = hours % 12 || 12;
+    // Convert 24-hour format to 12-hour format
+    const formattedHours = hours % 12 || 12;
 
-  // Add leading zeros to minutes and seconds if needed
-  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+    // Add leading zeros to minutes and seconds if needed
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
 
-  // Combine the formatted time components
-  const formattedTime = `${formattedHours}:${formattedMinutes}:${formattedSeconds} ${ampm}`;
+    // Combine the formatted time components
+    const formattedTime = `${formattedHours}:${formattedMinutes}:${formattedSeconds} ${ampm}`;
 
-  return formattedTime;
+    return formattedTime;
 }
 
 const clickOutside = () => {
-  showCard.value = false;
+    showCard.value = false;
+};
+
+const showCardtrue = async() => {
+    showCard.value = true;
+}
+
+const scrollEvent = ref()
+
+
+const toEvent  = async() => {
+
+  if (scrollEvent.value) {
+
+      const element = scrollEvent.value.getBoundingClientRect();
+
+      // Scroll to the element with smooth animation
+      window.scrollTo({
+          top: element.top + window.scrollY,
+          behavior: 'smooth',
+      });
+  }
+
 };
 
 
